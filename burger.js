7f4f18b2dc4ef4d3ee13cfe3ff94b8309651715c
naelyar <naelya@gmail.com>
2023-10-02 16:46:25 +0600

    f

--- burger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".headerRight");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
